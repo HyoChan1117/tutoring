2f409dd79ae419f5b99e2973c14139215116c000
Kim Hyo Chan <skkhc0317@gmail.com>
2026-04-02 17:36:28 +0900

    26.04.02. Update python lab answer

--- python/flow_control/lab_answer/2.py
+++ python/flow_control/lab_answer/2.py
@@ -51,3 +51,29 @@
 입력하신 변의 길이로는 삼각형을 만들 수 없습니다.
 삼각형을 만들기 위해서는 어떤 두 변의 길이의 합이 다른 한 변의 길이보다 커야 합니다.
 """
+
+# 사용자로부터 세 변의 길이를 입력 받는다.
+length1 = int(input("첫 번째 변의 길이를 입력하세요: "))
+length2 = int(input("두 번째 변의 길이를 입력하세요: "))
+length3 = int(input("세 번째 변의 길이를 입력하세요: "))
+
+# 조건에 따라 삼각형의 유형을 출력한다.
+# 세 변의 길이가 모두 같은 경우: 정삼각형
+if (length1 == length2 and length2 == length3 and length1 == length3):
+    print("입력하신 변의 길이로는 정삼각형을 만들 수 있습니다.")
+
+# 어느 두 변의 길이 합이 나머지 한 변의 길이보다
+# 작거나 같은 경우: 삼각형을 형성할 수 없음
+elif (length1 + length2 <= length3 or
+      length1 + length3 <= length2 or
+      length2 + length3 <= length1):
+    print("입력하신 변의 길이로는 삼각형을 만들 수 없습니다.")
+    print("삼각형을 만들기 위해서는 어떤 두 변의 길이의 합이 다른 한 변의 길이보다 커야 합니다.")
+
+# 세 변 중 두 변의 길이가 같은 경우: 이등변삼각형
+elif (length1 == length2 or length2 == length3 or length1 == length3):
+    print("입력하신 변의 길이로는 이등변삼각형을 만들 수 있습니다.")
+
+# 세 변의 길이가 모두 다른 경우: 부등변삼각형
+else:
+    print("입력하신 변의 길이로는 부등변삼각형을 만들 수 있습니다.")
